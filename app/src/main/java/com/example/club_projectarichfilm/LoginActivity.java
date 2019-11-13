4f8d14f8dcbeb1c34d62ef7b9e3f5b9918e0e720
100seo0 <qortjdud0627@naver.com>
2019-11-13 19:50:39 +0900

    commit LoginActivity

--- app/src/main/java/com/example/club_projectarichfilm/LoginActivity.java
+++ app/src/main/java/com/example/club_projectarichfilm/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.club_projectarichfilm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText et_email = (EditText) findViewById(R.id.et_email);
+        EditText et_pw = (EditText) findViewById(R.id.et_pw);
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+
+
+
     }
 }
