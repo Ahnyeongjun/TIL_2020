6bdfae3e6895206fff4f16ed0732820502d3cd1f
user <toinin@naver.com>
2020-07-27 15:08:16 +0900

    reboot

--- EX_APP/src/main/java/com/example/demo/ExApp2Application.java
+++ EX_APP/src/main/java/com/example/demo/ExApp2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExAppApplication {
+public class ExApp2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExAppApplication.class, args);
+		SpringApplication.run(ExApp2Application.class, args);
 	}
 
 }
